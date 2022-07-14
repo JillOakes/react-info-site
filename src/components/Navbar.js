e77cd1d5535076fdce911c306d55ed6835ef0033
Jill Oakes <jill.oakes@beckshybrids.com>
2022-07-14 14:24:55 -0400

    Complete static info page

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="./logo192.png" alt="react logo" className="icon" />
+            <h3 className="logo_text">ReactFacts</h3>
+            <h4 className="title">React Course - Project 1</h4>
+        </nav>
+    )
+}
